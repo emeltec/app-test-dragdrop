af24e8a5004d9fba06f69819296d2755a427b5c2
IBLINETUM\emilio.melgarejo <emilio.melgarejo@inetum.com>
2023-07-26 21:15:17 -0500

    Formulario sortable con .row .col

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Board1Component } from './components/board1/board1.component';
 import { Board2Component } from './components/board2/board2.component';
+import { FormFluidComponent } from './components/form-fluid/form-fluid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Board1Component,
-    Board2Component
+    Board2Component,
+    FormFluidComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/form-fluid/form-fluid.component.ts
@@ -0,0 +1,128 @@
+import { Component, OnInit } from '@angular/core';
+import { UID } from 'src/app/functions/uid';
+var Sortable = require('src/assets/js/sortable.js');
+
+@Component({
+  selector: 'app-form-fluid',
+  templateUrl: './form-fluid.component.html',
+  styleUrls: ['./form-fluid.component.css']
+})
+export class FormFluidComponent implements OnInit {
+
+  isDragActive = false;
+  tools = [
+    { id: 11, cols: 1, css: 'col col-12' },
+    { id: 22, cols: 2, css: 'col col-6' },
+    { id: 33, cols: 3, css: 'col col-4' },
+    { id: 44, cols: 4, css: 'col col-3' },
+  ]
+  constructor() { }
+
+  ngOnInit() {
+
+
+  }
+
+  getAttrib(data: any) {
+    // console.log(data)
+    return JSON.stringify(data)
+  }
+
+  ngAfterViewInit() {
+    const dragItems: any = document.querySelector('#formFluid');
+    Sortable.create(dragItems, {
+      animation: 150,
+      ghostClass: 'sortable-ghost',
+      onSort: function (e: any) {
+        console.log(e);
+      }
+    })
+  }
+
+  onClick(e: any) {
+    console.log(e)
+  }
+
+  onDragstart(e: any) {
+    // e.preventDefault();
+    // e.stopPropagation();
+    let data = e.target.attributes['data-item'].value
+    e.dataTransfer.setData("text/plain", e.target.id);
+    e.dataTransfer.setData("data", data);
+    e.dataTransfer.effectAllowed = "move";
+
+    console.log('START', e.target.attributes)
+    // let c = JSON.parse(e.target.attributes.txt.value)
+  }
+
+
+
+  onDrop(e: any) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.isDragActive = false;
+    const id = e.dataTransfer.getData('text/plain');
+
+    const draggable: any = document.getElementById(id);
+    const data: any = JSON.parse(e.dataTransfer.getData('data'));
+    // console.log(data)
+
+    let isCopy = draggable.getAttribute('data-iscopy') || null;
+    console.log(draggable)
+
+    if (isCopy != 'true') {
+      let clone = draggable.cloneNode(true);
+      clone.setAttribute('data-iscopy', 'true')
+      clone.id = UID();
+      clone.setAttribute('class', data.css);
+      clone.addEventListener('dragstart', this.onDragstart);
+      e.target.appendChild(clone);
+    } else {
+      e.target.appendChild(draggable);
+    }
+
+
+
+    // e.target.appendChild(draggable);
+
+  }
+
+
+
+  onDrag(e: any) {
+    e.preventDefault();
+    e.stopPropagation();
+    e.dataTransfer.clearData();
+    e.dataTransfer.setData("text/plain", e.target.id);
+  }
+
+  onDragenter(e: any) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.isDragActive = true;
+    e.dataTransfer.clearData();
+    e.dataTransfer.setData("text/plain", e.target.id);
+  }
+
+
+
+  onDragOver(e: any) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  onDragleave(e: any) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.isDragActive = false;
+    console.log('LEAVE', e)
+  }
+
+  onDragend(e: any) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.isDragActive = false;
+    console.log('END', e)
+  }
+
+}
